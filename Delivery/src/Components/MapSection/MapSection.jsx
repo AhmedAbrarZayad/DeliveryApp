1acd5471fb65e655b8b8c2e7c8a2daa85fcfeb58
Ahmed Abrar <abrarjubah82@gmail.com>
2025-12-06 16:43:47 +0600

    Needed to do some other shit

--- Delivery/src/Components/MapSection/MapSection.jsx
+++ Delivery/src/Components/MapSection/MapSection.jsx
@@ -2,6 +2,15 @@ import React, { useEffect, useRef, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useOutletContext } from 'react-router';
+import L from 'leaflet';
+
+// Fix for default marker icons in production
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
 
 function FitToLocations({ locations }) {
     const map = useMap();
