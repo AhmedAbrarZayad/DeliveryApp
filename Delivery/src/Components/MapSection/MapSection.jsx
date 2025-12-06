7c15593f8e654a495b44f085107c7e1fa637c4bf
Ahmed Abrar <abrarjubah82@gmail.com>
2025-12-06 17:03:17 +0600

    Complete

--- Delivery/src/Components/MapSection/MapSection.jsx
+++ Delivery/src/Components/MapSection/MapSection.jsx
@@ -4,13 +4,22 @@ import 'leaflet/dist/leaflet.css';
 import { useOutletContext } from 'react-router';
 import L from 'leaflet';
 
-// Fix for default marker icons in production
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+// Fix for default marker icons in production - using cdnjs instead of unpkg
+const iconUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png';
+const iconRetinaUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png';
+const shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png';
+
+const defaultIcon = L.icon({
+    iconUrl,
+    iconRetinaUrl,
+    shadowUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
+
+L.Marker.prototype.options.icon = defaultIcon;
 
 function FitToLocations({ locations }) {
     const map = useMap();
